6df06ed79f79f147f8a716a39cc5a86b9ee42f28
agreyest <56709605+agreyest@users.noreply.github.com>
2020-06-12 18:45:24 -0600

    se hizo cambio en elefante

--- Elefante.cpp
+++ Elefante.cpp
@@ -4,3 +4,15 @@
 Elefante::Elefante(int i, int j, bool jug, char type):Pieza(i, j, jug, type){
 	
 }
+
+bool Elefante::valido(int i, int j, Tablero* tab){
+	if( ( (this->fila-2)==i && ( (this->columna-2)==j || (this->columna+2)==j) ) || 
+	    ( (this->fila+2)==i && ( (this->columna-2)==j || (this->columna+2)==j) ) ){
+		
+		
+			return true;
+		
+	}else{
+		return false;
+	}
+}
